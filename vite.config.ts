198170783a03ebcadc59248a33a9ed54fe3524c4
sacru2red <sacru2red@gmail.com>
2025-07-13 21:43:05 +0900

    rename html file

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    open: '/quux_hidden_app.html',
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        app: '.quux_hidden_app.html',
+      },
+    },
+  },
 })
