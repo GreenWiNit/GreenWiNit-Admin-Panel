79bd6fdf2bd9e4ae6356729833ddb70382883a51
sacru2red <sacru2red@gmail.com>
2025-07-15 02:08:09 +0900

    follow guideline

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      // routeFilePrefix: 'quux_hidden_app',
+      // addExtensions: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   server: {
-    open: './quux_hidden_app.html',
+    // open: './quux_hidden_app.html',
+    // base: '/quux_hidden_app',
   },
   build: {
     rollupOptions: {
