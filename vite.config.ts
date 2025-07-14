248288ece50e58a7857d7d39fa419abb276b6c05
sacru2red <sacru2red@gmail.com>
2025-07-15 00:29:15 +0900

    fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    open: '/.quux_hidden_app.html',
+    open: './quux_hidden_app.html',
   },
   build: {
     rollupOptions: {
