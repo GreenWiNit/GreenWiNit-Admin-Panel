30583fc550d17fcd72e8863090000ad903f7732f
sacru2red <sacru2red@gmail.com>
2025-07-18 06:16:18 +0900

    로그인 화면 스타일 조정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,13 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import '../index.css'
+import { Fragment } from 'react'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="flex gap-2 p-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-        <Link to="/login" className="[&.active]:font-bold">
-          Login
-        </Link>
-      </div>
-      <hr />
+    <Fragment>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </Fragment>
   ),
 })
