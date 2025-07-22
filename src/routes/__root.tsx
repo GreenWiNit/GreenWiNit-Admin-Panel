828ceb9685cb144cfdc6eb0f8aa03c130afba1c4
sacru2red <sacru2red@gmail.com>
2025-07-22 18:34:04 +0900

    chore: setup msw following https://mswjs.io/docs/quick-start, https://mswjs.io/docs/integrations/browser

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,17 @@ import '../index.css'
 import { Fragment } from 'react'
 
 export const Route = createRootRoute({
+  beforeLoad: async () => {
+    if (import.meta.env.MODE === 'production') {
+      return
+    }
+
+    const { worker } = await import('../mocks/browser')
+
+    // `worker.start()` returns a Promise that resolves
+    // once the Service Worker is up and ready to intercept requests.
+    return worker.start()
+  },
   component: () => (
     <Fragment>
       <Outlet />
