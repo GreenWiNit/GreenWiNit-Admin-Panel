ccaf43ec79904a4b893acb00a07db85ad1859122
sacru2red <sacru2red@gmail.com>
2025-08-03 19:54:55 +0900

    fix: build fail invalid ref (related msw)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,17 +10,6 @@ import { initHistoryAndLocation } from '@/lib/utils'
 const queryClient = new QueryClient()
 
 export const Route = createRootRoute({
-  beforeLoad: async () => {
-    if (import.meta.env.MODE === 'production') {
-      return
-    }
-
-    const { worker } = await import('../mocks/browser')
-
-    // `worker.start()` returns a Promise that resolves
-    // once the Service Worker is up and ready to intercept requests.
-    return worker.start()
-  },
   component: () => (
     <QueryClientProvider client={queryClient}>
       <Outlet />
