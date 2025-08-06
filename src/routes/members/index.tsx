12f19bf141e07a32a5c8c4b625d2833d0e70f19b
정준영 <wn8624@naver.com>
2025-08-06 21:39:13 +0900

    feat: publish members page

---
+++ src/routes/members/index.tsx
@@ -0,0 +1,99 @@
+import GlobalNavigation from '@/components/global-navigation'
+import dayjs from 'dayjs'
+import PageContainer from '@/components/page-container'
+import PageTitle from '@/components/page-title'
+import FilePresentIcon from '@mui/icons-material/FilePresent'
+import { Separator } from '@radix-ui/react-select'
+import { createFileRoute } from '@tanstack/react-router'
+import { DataGrid, type GridColDef } from '@mui/x-data-grid'
+import type { MemberData } from '@/api/member'
+import { Button } from '@/components/shadcn/button'
+import { useActiveMembers } from '@/hooks/use-members'
+
+export const Route = createFileRoute('/members/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const { data } = useActiveMembers()
+  if (!data) return <div>데이터를 불러오는 중...</div> //@TODO fallback ui로 대체될 예정 (기획 미정)
+  if (!data.result?.content) return <div>리스트가 없습니다..</div>
+
+  const downloadExcel = () => {
+    // @TODO 엑셀 다운로드 api 연결 예정
+  }
+
+  return (
+    <PageContainer className="flex-row">
+      <GlobalNavigation />
+      <div className="flex w-full flex-col gap-4">
+        <PageTitle className="self-start">회원 목록</PageTitle>
+        <Separator />
+        <div className="flex justify-between">
+          <span className="text-2xl">Title : {data.result?.totalElements}</span>
+          <Button className="w-fit" onClick={downloadExcel}>
+            <FilePresentIcon />
+            엑셀 받기
+          </Button>
+        </div>
+        <div className="flex w-full">
+          <DataGrid
+            rows={data.result?.content.map((item) => ({
+              ...item,
+              id: item.memberKey,
+              joinDate: dayjs(item.joinDate).format('YYYY-MM-DD'),
+            }))}
+            columns={columns}
+            initialState={{
+              pagination: { paginationModel: { page: 0, pageSize: 10 } },
+            }}
+          />
+        </div>
+      </div>
+    </PageContainer>
+  )
+}
+
+const columns: GridColDef<MemberData>[] = [
+  { field: 'memberKey', headerName: 'MemberKey', flex: 1, headerAlign: 'center', align: 'center' },
+  { field: 'email', headerName: '이메일', flex: 1, headerAlign: 'center', align: 'center' },
+  { field: 'nickname', headerName: '닉네임', flex: 1, headerAlign: 'center', align: 'center' },
+  { field: 'phoneNumber', headerName: '전화번호', flex: 1, headerAlign: 'center', align: 'center' },
+  { field: 'joinDate', headerName: '가입일', flex: 1, headerAlign: 'center', align: 'center' },
+  { field: 'role', headerName: '등급', flex: 1, headerAlign: 'center', align: 'center' },
+  {
+    field: 'actions',
+    headerName: '관리',
+    headerAlign: 'center',
+    align: 'center',
+    flex: 1,
+    sortable: false,
+    renderCell: () => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <button
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '12px',
+            width: '60px',
+            height: '28px',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            cursor: 'pointer',
+          }}
+        >
+          삭제
+        </button>
+      </div>
+    ),
+  },
+]
